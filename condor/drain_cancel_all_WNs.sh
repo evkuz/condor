cd483dbffe88cebff31fa22654cb0c0804c6cd60
E. Kuznetsov <evkuz@jinr.ru>
2020-03-30 15:11:47 +0300

    30.03.2020 1st commit to github from cm since native git is in trouble

--- condor/drain_cancel_all_WNs.sh
+++ condor/drain_cancel_all_WNs.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# make "condor_drain -graceful" for all worknodes in the pool
+# make "condor_drain -cancel" for all worknodes in the pool
 
 SOURCE_FILE="vms_fqdn.lst"
+OUTPUT_FILE="result_of_ddrain_cancel.txt"
 
 iparray=($(cat $SOURCE_FILE))
 
 for index in ${!iparray[*]}
 do
-     condor_drain -cancel ${iparray[$index]}
+     condor_drain -cancel ${iparray[$index]} > $OUTPUT_FILE
 done
 echo "Done"
 
